e41efc226fbe5a7409098636bedd1ac78bdf8531
kimalizadeh <kim.alizadeh@gmail.com>
2021-09-19 20:19:46 -0400

    updated ingredients script

--- Taste_of_Baking_website/scripts/ingredients.js
+++ Taste_of_Baking_website/scripts/ingredients.js
@@ -3,6 +3,8 @@ var recipeQuantities = [];
 
 var myList = document.querySelectorAll("#ingredients ul li");
 
+var servings = document.getElementById('servings').innerHTML;
+
 for (let i = 0; i < myList.length; i++) {
     let mySplit = myList[i].innerHTML.split(" ");
     recipeQuantities[i] = parseFloat(mySplit[0]);
@@ -10,19 +12,25 @@ for (let i = 0; i < myList.length; i++) {
 
 function updateRecipe() {
     let newQuantities = [];
+    let newServings;
     var amount = proportion.options[proportion.selectedIndex].value;
 
 
     if (amount == '2') {
         newQuantities = recipeQuantities.map(x => x * 2);
+        newServings = servings * 2;
+        
+        
 
     }
     else if (amount == '0.5') {
         newQuantities = recipeQuantities.map(x => x * 0.5);
+        newServings = servings * 0.5;
 
     }
     else {
         newQuantities = recipeQuantities;
+        newServings = servings;
     }
 
     console.log(recipeQuantities);
@@ -38,6 +46,8 @@ function updateRecipe() {
         let ingredient = mySplit.join(" ");
         myList[i].innerHTML = ingredient;
     }
+
+    document.getElementById('servings').innerHTML = newServings
 };
 
 
